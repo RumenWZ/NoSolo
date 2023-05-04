17f326eacd5a8e1dbce0e8f5c397fd5d7195c998
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2023-05-04 15:30:21 +0300

    added styling for sidebar menu items

---
+++ FrontendNS/src/app/sidebar/sidebarMenuEntries.ts
@@ -0,0 +1,12 @@
+export const sidebarMenuEntries = [
+  {
+    'label': 'Dashboard',
+    'icon': 'bx bx-home',
+    'functionName': 'onDashboard'
+  },
+  {
+    'label': 'Logout',
+    'icon': 'bx bx-exit',
+    'functionName': 'onLogout',
+  },
+]
