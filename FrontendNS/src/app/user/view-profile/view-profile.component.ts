1097e99d14efcd9ca55a53521e7ea30b5f541ed6
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2023-06-23 15:50:58 +0300

    added validation for invalid users on view-profile

--- FrontendNS/src/app/user/view-profile/view-profile.component.ts
+++ FrontendNS/src/app/user/view-profile/view-profile.component.ts
@@ -13,11 +13,15 @@ export class ViewProfileComponent {
   token: string;
   user: UserDTO;
   isMyOwnProfile: boolean;
+  myUsername: string;
+  isValidUsername: boolean;
 
   constructor(
     private route: ActivatedRoute,
-    private userService: UserService) {
+    private userService: UserService
+    ) {
     this.token = localStorage.getItem('token');
+    this.myUsername = localStorage.getItem('userName');
   }
 
   onSendFriendRequest() {
@@ -26,21 +30,20 @@ export class ViewProfileComponent {
 
   ngOnInit() {
     this.username = this.route.snapshot.paramMap.get('username');
-
-    const cachedUser = this.userService.getCachedUser();
-
-    if (cachedUser) {
-      console.log('no need for API call');
-      this.user = cachedUser;
-    } else {
-      this.userService.getUserByToken(this.token).subscribe(
-        (response: UserDTO) => {
-          this.user = response;
-          this.isMyOwnProfile = this.username === this.user.username ? true : false;
-        }
-      );
-    }
+    
+    this.userService.getUserByUsername(this.username).subscribe((response: any) => {
+      if (response.error) {
+        this.isValidUsername = false;
+      } else {
+        this.isValidUsername = true;
+        this.user = response;
+      if (response.profileImageUrl == '') {
+        this.user.profileImageUrl = '/assets/images/default-user.png';
+        this.isMyOwnProfile = this.username == this.myUsername ? true : false;
+      }
+      }
 
 
+    })
   }
 }
