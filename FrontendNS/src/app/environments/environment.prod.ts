60abc140ad7db29e3825eb00db1962c42651e7a5
RumenWZ <98177567+RumenWZ@users.noreply.github.com>
2024-01-29 18:19:12 +0200

    test

--- FrontendNS/src/app/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  pusherAppId: "1631650",
-  pusherKey: "e35d41811e1a7a4248fa",
-  pusherSecret: "378e73d88392e7d7e2b7",
-  pusherCluster: "eu",
-  apiUrl: "https://rumenwz-001-site1.etempurl.com/api"
-};
